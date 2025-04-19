6e300731fac785254ad506092c1c9faf3cf5a302
Ord00 <ivansimonov7@yandex.ru>
2025-04-20 01:47:48 +0300

    Added tests for service

--- src/main/java/vacation/calculator/dto/VacationPayRequest.java
+++ src/main/java/vacation/calculator/dto/VacationPayRequest.java
@@ -1,14 +1,17 @@
 package vacation.calculator.dto;
 
 import lombok.AllArgsConstructor;
-import lombok.Data;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
 
 @AllArgsConstructor
+@NoArgsConstructor
 @Getter
+@Setter
 public class VacationPayRequest {
 
     private BigDecimal avgSalary;
--- src/main/java/vacation/calculator/dto/VacationPayResponse.java
+++ src/main/java/vacation/calculator/dto/VacationPayResponse.java
@@ -2,11 +2,15 @@ package vacation.calculator.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.math.BigDecimal;
 
 @AllArgsConstructor
+@NoArgsConstructor
 @Getter
+@Setter
 public class VacationPayResponse {
 
     private BigDecimal amount;
